1f9ee95fae990a4c602d16d1284cd53dc5c27c9c
huyingzhao <13824314620@139.com>
2023-05-03 07:37:07 +0800

    update

--- hyz-spring-boot/src/test/java/ListTest.java
+++ hyz-spring-boot/src/test/java/ListTest.java
@@ -1,10 +1,9 @@
-package collection;
-
 /**
  * @author add by huyingzhao
  * 2023-03-18 13:26
  */
 public class ListTest {
+
     public void list(){
         // List<String> strings=new ArrayList<>();
         // ListIterator<String> eListIterator= strings.listIterator();
